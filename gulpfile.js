72cb1dfbf34a4e34146a73d246519d7f74208a1d
Nic Ficca <nicficca@gmail.com>
2017-01-24 17:14:49 -0500

    reverted gulpfile to es5 standard

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-const gulp = require('gulp'),
-        babel = require('gulp-babel'),
-        uglify = require('gulp-uglify'),
-        nodemon = require('gulp-nodemon'),
-        jsdoc = require('gulp-jsdoc-json');
+var gulp    = require('gulp'),
+    babel   = require('gulp-babel'),
+    uglify  = require('gulp-uglify'),
+    nodemon = require('gulp-nodemon'),
+    jsdoc   = require('gulp-jsdoc-json');
 
-const paths = {
+var paths = {
     scripts: ['src/**/*.js'],
     commands: ['src/commands/**/*.js']
 };
@@ -13,20 +13,20 @@ gulp.task('default', ['scripts', 'docs', 'start']);
 
 gulp.task('prod', ['scripts', 'docs']);
 
-gulp.task('scripts', () => {
+gulp.task('scripts', function() {
     return gulp.src(paths.scripts)
         .pipe(babel({presets: ['es2015']}))
         .pipe(uglify())
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('docs', () => {
+gulp.task('docs', function() {
     return gulp.src(paths.commands)
         .pipe(jsdoc({output: 'docs.json'}))
         .pipe(gulp.dest('dist/docs/'));
 });
 
-gulp.task('start', () => {
+gulp.task('start', function() {
    nodemon({
        script: 'dist/bot.js',
        watch: paths.scripts,
